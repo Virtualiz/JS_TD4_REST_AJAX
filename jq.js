7cabf37c5d66e5a8db8471da5b67c5d65acc9f97
JusticeNezuro <justin.noel5@etu.univ-lorraine.fr>
2016-03-07 09:43:44 +0100

    Ajout de fonctions dans le js

--- jq.js
+++ jq.js
@@ -1,3 +1,39 @@
 /**
  * Created by Bruno on 07/03/2016.
  */
+var prolunch = {modules : {}};
+
+prolunch.modules.itemManager = (function () {
+    return {
+        registerGetPlatHandler : function (plat, e) {
+
+        },
+        registerBackToListHandler : function () {
+
+        },
+
+        service : (function () {
+            return {
+                getResource : function (uri, callback) {
+
+                }
+            }
+        }) (),
+
+        view : (function () {
+            return {
+                displayPlat : function (data) {
+
+                },
+                displayListe : function (data) {
+
+                }
+            }
+        }) ()
+    }
+}) ();
+
+
+prolunch.init = function () {
+
+};
