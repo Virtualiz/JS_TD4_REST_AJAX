2f2699849e6810e9dcc9e07733ff928d842e8769
JusticeNezuro <justin.noel5@etu.univ-lorraine.fr>
2016-03-16 10:42:53 +0100

    Début d'ajout d'un module pour afficher un plat

--- jq.js
+++ jq.js
@@ -23,7 +23,11 @@ prolunch.modules.itemManager = (function () {
         view : (function () {
             return {
                 displayPlat : function (data) {
-
+                    var id = data.plat.id;
+                    var nom = data.plat.nom;
+                    var description = data.plat.description;
+                    var prix = data.plat.prix;
+                    var res = "<div class=\"col-sm-6 col-md-4 col-lg-4\"> <div class=\"thumbnail\"> <img class=\"img-rounded img-responsive\" src=\"https://webetu.iutnc.univ-lorraine.fr/www/canals5/prolunch/images/original/boeufChampignonsNoirs.jpg\" alt=\"...\"> <div class=\"caption text-center\"> <h3>Titre vraiment très très très très très très très long</h3> <p>Description du plat qui se trouve être plutôt très très très très très très très très très très longue.</p> <p><a href=\"#\" class=\"btn btn-primary\" role=\"button\">Ajouter au panier <span class=\"badge\">6.20€</span></a></p> </div> </div> </div>";
                 },
                 displayListe : function (data) {
 
@@ -37,3 +41,22 @@ prolunch.modules.itemManager = (function () {
 prolunch.init = function () {
 
 };
+
+var lePlat = {
+    plat: {
+        id: 4,
+        nom: "Donkey Kong Pizza ",
+        description: "Sauce Barbecue, mozzarella, Tomate, Chorizo, Pepperoni.",
+        prix: "12.00",
+        photo: {
+            href: "/images/original/Pizza_donkey.jpg"
+        },
+        id_resto: 1,
+        type: "plat"
+    },
+    _links: {
+        resto: "/plats/4/resto"
+    }
+};
+
+prolunch.modules.itemManager.view.displayPlat(lePlat);
